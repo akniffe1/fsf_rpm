c85a265a61a15f51919c0d2a3e2e5eda18c8c672
Adam Kniffen <adamkniffen@gmail.com>
2017-01-25 19:26:11 +0000

    more cleanup and organization on the directory, plus a fresh fpm build script

--- build_fsf.sh
+++ build_fsf.sh
@@ -1,2 +1,38 @@
-/usr/local/share/gems/gems/fpm-1.8.0/bin/fpm -s dir -t rpm -n fsfserver -v 1 --iteration 1 -d "python-concurrentloghandler" -d "python-czipfile" -d "python-hachoir-core" -d " python-hachoir-metadata" -d "python-hachoir-parser" -d "python-hachoir-regex" -d "python-hachoir-subfile" -d "python-javatools" -d "python-oletools" -d "python-pefile" -d "python-pylzma" -d "python-pypdf2" -d "python-ssdeep" -d "autoconf" -d "python-devel" -d "automake" -d "libtool" -d "openssl" -d "openssl-devel" -d "net-tools" -d "ssdeep-devel" -d "libffi-devel" -d "unrar" -d "upx" -d "unzip" -d "cabextract" -d "macholibre" -d "python-ctypescrypto" -d "yara == 3.4.0" --post-install ./fsf/post-install.sh ./fsf=/opt/ ./fsf/init.d/fsf.service=/etc/systemd/system/fsf.service
+/usr/local/share/gems/gems/fpm-1.8.0/bin/fpm -s dir -t rpm -n fsfserver -v 1 --iteration 1 \
+-d "python-concurrentloghandler" \
+-d "python-czipfile" \
+-d "python-hachoir-core" \
+-d "python-hachoir-metadata" \
+-d "python-hachoir-parser" \
+-d "python-hachoir-regex" \
+-d "python-hachoir-subfile" \
+-d "python-javatools" \
+-d "python-oletools" \
+-d "python-pefile" \
+-d "python-pylzma" \
+-d "python-pypdf2" \
+-d "python-ssdeep" \
+-d "python-requests" \
+-d "python-pyasn1-modules" \
+-d "python-pyelftools" \
+-d "python-rarfile" \
+-d "python-xmltodict" \
+-d "python-ctypescrypto" \
+-d "macholibre" \
+-d "autoconf" \
+-d "python-devel" \
+-d "automake" \
+-d "libtool" \
+-d "openssl" \
+-d "openssl-devel" \
+-d "net-tools" \
+-d "ssdeep-devel" \
+-d "libffi-devel" \
+-d "unrar" \
+-d "upx" \
+-d "unzip" \
+-d "cabextract" \
+-d "yara == 3.4.0" \
+--post-install ./post-install.sh \
+./yara/=/var/lib/yara-rules/ ./fsf/=/opt/fsf ./init.d/fsf.service=/etc/systemd/system/fsf.service
 
--- post-install.sh
+++ post-install.sh
@@ -5,33 +5,37 @@ FSF_ARCHIVEDIR=/data/fsf/archive
 YARA_RULEDIR=/var/lib/yara-rules
 FSF_SRCDIR=/opt/fsf
 
+# drop the systemd service file
+#if [ ! -a /etc/systemd/system/fsf.service ]; then cp ./init.d/fsf.service /etc/systemd/system/
+#fi
+
 # set up Yara rules directory
-if ! id "fsf" >/dev/null 2>&1; then useradd fsf
-fi 
+#if ! id "fsf" >/dev/null 2>&1; then useradd fsf
+#fi 
 
 # set up $YARA_RULEDIR
 
-if [ ! -d $YARA_RULEDIR ]; then sudo -u fsf && kdir -m 744 $YARA_RULEDIR
+if [ ! -d $YARA_RULEDIR ]; then mkdir -m 744 $YARA_RULEDIR && chown -R fsf:fsf $YARA_RULEDIR
 fi 
 
 # set up $FSF_LOGDIR
 
-if [ ! -d $FSF_LOGDIR ]; then sudo -u fsf && mkdir -m 0755 $FSF_LOGDIR
+if [ ! -d $FSF_LOGDIR ]; then mkdir -m 0755 $FSF_LOGDIR && chown -R fsf:fsf $FSF_LOGDIR
 fi
 
 # set up $FSF_ARCHIVEDIR
 
-if [ ! -d $FSF_ARCHIVEDIR ]; then sudo -u fsf && mkdir -m 0755 $FSF_ARCHIVEDIR
+if [ ! -d $FSF_ARCHIVEDIR ]; then mkdir -m 0755 $FSF_ARCHIVEDIR && chown -R fsf:fsf $FSF_ARCHIVEDIR
 fi
 
 # finally change ownership of the $FSF_SCRDIR
-chown -R fsf:fsf $FSF_SRCDIR
+#chown -R fsf:fsf $FSF_SRCDIR
 
-chown -R fsf:fsf $YARA_RULEDIR
+#chown -R fsf:fsf $YARA_RULEDIR
 
 # symlink the server start and client
-if [ ! -e $FSF_SRCDIR/fsf_server/main.py ]; then ln -s $FSF_SRCDIR/fsf_server/main.py /usr/bin/fsfserver
-fi 
+#if [ ! -e $FSF_SRCDIR/fsf_server/main.py ]; then ln -s $FSF_SRCDIR/fsf_server/main.py /usr/bin/fsfserver
+#fi 
 
 if [ ! -e $FSF_SRCDIR/fsf_client/fsf_client.py ]; then ln -s $FSF_SRCDIR/fsf_client/fsf_client.py /usr/bin/fsfclient
 fi
