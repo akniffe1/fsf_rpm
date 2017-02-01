c91a88a2f892d55c261846c221029d0c4ee6873f
akniffe1 <adamkniffen@gmail.com>
2017-02-01 11:41:34 -0600

    updated post install to ensure that the fsf files are owned by fsf user and group

--- post-install.sh
+++ post-install.sh
@@ -8,23 +8,23 @@ FSF_SRCDIR=/opt/fsf
 
 # set up $YARA_RULEDIR
 
-if [ ! -d $YARA_RULEDIR ]; then mkdir -m 744 $YARA_RULEDIR && chown -R fsf $YARA_RULEDIR
+if [ ! -d $YARA_RULEDIR ]; then mkdir -m 744 $YARA_RULEDIR && chown -R fsf:fsf $YARA_RULEDIR
 fi 
 
 # set up $FSF_LOGDIR
 if [ ! -d /data ]; then mkdir -m 0644 /data
 fi
 
-if [ ! -d $FSF_LOGDIR ]; then mkdir -m 644 $FSF_LOGDIR && chown -R fsf $FSF_LOGDIR
+if [ ! -d $FSF_LOGDIR ]; then mkdir -m 644 $FSF_LOGDIR && chown -R fsf:fsf $FSF_LOGDIR
 fi
 
 # set up $FSF_ARCHIVEDIR
 
-if [ ! -d $FSF_ARCHIVEDIR ]; then mkdir -m 644 $FSF_ARCHIVEDIR && chown -R fsf $FSF_ARCHIVEDIR
+if [ ! -d $FSF_ARCHIVEDIR ]; then mkdir -m 644 $FSF_ARCHIVEDIR && chown -R fsf:fsf $FSF_ARCHIVEDIR
 fi
 
 # set permissions on the $FSF_SRCDIR
-chown -R fsf $FSF_SRCDIR
+chown -R fsf:fsf $FSF_SRCDIR
 
 # set proper file attribute on systemd file
 if [ -e /etc/systemd/system/fsf.service ]; then chmod 644 /etc/systemd/system/fsf.service
