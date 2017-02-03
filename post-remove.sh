c8d26ded0fe584d27fcde632c17af18f0058d493
dcode <dcode@users.noreply.github.com>
2017-02-03 00:11:34 -0600

    Remove RUNDIR on package uninstall

--- post-remove.sh
+++ post-remove.sh
@@ -4,9 +4,13 @@ FSF_LOGDIR=/var/lib/fsf
 FSF_ARCHIVEDIR=/var/lib/fsf/archive
 YARA_RULEDIR=/var/lib/yara-rules
 FSF_SRCDIR=/opt/fsf
+FSF_RUNDIR=/run/fsf
 
 # remove $FSF_SRCDIR
 rm -rf $FSF_SRCDIR
+
+# remove RUNDIR
+rm -rf $FSF_RUNDIR
 
 # remove the client and server symlinks in /usr/local/bin/
 if [ -e /usr/local/bin/fsfclient ]; then rm /usr/local/bin/fsfclient
